9417b165187717aa2e34271e39b663d387fb8298
Nadeesha Madusanka <110985815+nadeeshaMadusanka99@users.noreply.github.com>
2023-10-12 13:05:23 +0530

    Implemented route functions for the new search types

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -4,19 +4,26 @@ import { Fragment, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Listbox, Transition } from "@headlessui/react";
-import { CustomFilterProps } from "@types";
-import { updateSearchParams } from "@utils";
+import { CustomFilterProps } from "@/types";
+import { updateSearchParams } from "@/utils";
 
-const CustomFilter = ({title , options}) => {
+const CustomFilter = ({title , options}:CustomFilterProps) => {
+  const router = useRouter();
   const [selected, setSelected] = useState(options[0]); // State for storing the selected option
 
+  // update the URL search parameters and navigate to the new URL
+  const handleUpdateParams = (e: { title: string; value: string }) => {
+    const newPathName = updateSearchParams(title, e.value.toLowerCase());
+
+    router.push(newPathName);
+  };
   return (
     <div className='w-fit'>
       <Listbox
         value={selected}
         onChange={(e) => {
           setSelected(e); // Update the selected option in state
-          // handleUpdateParams(e); // Update the URL search parameters and navigate to the new URL
+          handleUpdateParams(e); // Update the URL search parameters and navigate to the new URL
         }}
       >
         <div className='relative w-fit z-10'>
--- types/index.ts
+++ types/index.ts
@@ -47,3 +47,4 @@ export interface CustomFilterProps {
   title :string;
   options : OptionProps[];
 }
+
